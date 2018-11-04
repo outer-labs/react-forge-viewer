1ee27e5c24c44ba8f548b90539fa2673a901948d
Karl Garske <karl@Karls-MacBook-Pro-2.local>
2018-11-04 14:51:51 -0800

    Setting default width and height of viewer to 100%

--- src/index.js
+++ src/index.js
@@ -279,33 +279,30 @@ class ForgeViewer extends React.Component {
             onError={this.handleViewerError.bind(this)}
           />
 
-          {this.state.empty ?
+          {this.state.empty &&
             <div className="scrim">
               <div className="message">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12.89 1.45l8 4A2 2 0 0 1 22 7.24v9.53a2 2 0 0 1-1.11 1.79l-8 4a2 2 0 0 1-1.79 0l-8-4a2 2 0 0 1-1.1-1.8V7.24a2 2 0 0 1 1.11-1.79l8-4a2 2 0 0 1 1.78 0z"></path><polyline points="2.32 6.16 12 11 21.68 6.16"></polyline><line x1="12" y1="22.76" x2="12" y2="11"></line></svg>
               </div>
             </div>
-            : null
           }
 
-          {this.state.error ?
+          {this.state.error &&
             <div className="scrim">
               <div className="message">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12" y2="16"></line></svg>
                 <div>Viewer Error</div>
               </div>
             </div>
-            : null
           }
 
-          {!this.state.enable ?
+          {!this.state.enable &&
             <div className="scrim">
               <div className="message">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon></svg>
                 <div>Starting Viewer...</div>
               </div>
             </div>
-            : null
           }
         </div>
         }
